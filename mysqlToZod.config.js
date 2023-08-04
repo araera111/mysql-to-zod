e86899ad22c5bf463574b9b78afadd2115232094
yuumillar <araera111@gmail.com>
2023-08-05 00:33:52 +0900

    feat:

--- mysqlToZod.config.js
+++ mysqlToZod.config.js
@@ -1,8 +1,23 @@
 const options = {
   outFilePath: "mysqlToZod",
   fileName: "schema.ts",
+  comments: {
+    table: {
+      active: true,
+      format: "// [table:!name] : !text",
+    },
+    column: {
+      active: true,
+      format: "// !name : !text",
+    },
+  },
   schema: {
-    zod: ["DATETIME", "z.string()"],
+    format: "camel",
+    prefix: "",
+    suffix: "Schema",
+    replacements: [],
+    nullType: "nullish",
+    zod: [["DATETIME", "z.string()"]],
   },
 };
 module.exports = options;
