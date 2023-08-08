b83ba4423bd04ff83967541b462337edc1d0a9e9
yuumillar <araera111@gmail.com>
2023-08-08 13:46:54 +0900

    feat:

--- src/process/buildSchemaText/utils/createSchemaFile.ts
+++ src/process/buildSchemaText/utils/createSchemaFile.ts
@@ -28,7 +28,9 @@ export const createSchemaFile = (
   const parser = new Parser();
   const [tableName, tableDefinitionString] = tableDefinition;
   if (isNil(tableName) || isNil(tableDefinitionString))
-    return left("createSchemaFileError. tableName or tableDefinitionString is nil");
+    return left(
+      "createSchemaFileError. tableName or tableDefinitionString is nil",
+    );
   const ast = parser.astify(tableDefinitionString);
   if (Array.isArray(ast) || !isCreate(ast))
     return left("createSchemaFileError ast parser error");
