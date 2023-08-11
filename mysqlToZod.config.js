97ed775cc3032e342aee402e9bc6971c3dd16f79
yuumillar <araera111@gmail.com>
2023-08-11 22:24:36 +0900

    fix(type.ts): delete declared:interface
    fix #74

--- mysqlToZod.config.js
+++ mysqlToZod.config.js
@@ -10,13 +10,20 @@ const options = {
       format: "// !name : !text",
     },
   },
+  type: {
+    declared: "interface",
+    format: "pascal",
+    prefix: "",
+    suffix: "",
+    replacements: [],
+  },
   schema: {
     format: "camel",
     prefix: "",
     suffix: "Schema",
     replacements: [],
     nullType: "nullish",
-    inline: true,
+    inline: false,
     zod: {
       implementation: [["DATETIME", "z.string()"]],
       references: [["DATETIME", "ourDateTime"]],
