ed2edcc18bf841b8aee8455f21532df0df1f5818
yuumillar <araera111@gmail.com>
2023-08-11 21:02:51 +0900

    feat(options.ts): delete old otpions
    feat #66

--- src/options/options.ts
+++ src/options/options.ts
@@ -4,19 +4,16 @@ import {
   defaultTableCommentFormat,
   optionCommentsSchema,
 } from "./comments";
-import { nullTypeUnionSchema } from "./common";
 import { SchemaOptionSchema } from "./schema";
 import { typeOptionSchema } from "./type";
 
 export const mysqlToZodOptionSchema = z.object({
-  isAddType: z.boolean().optional(), // I hope to have it DEPRECATED in the near future.
-  isCamel: z.boolean().optional(), // I hope to have it DEPRECATED in the near future.
-  isTypeUpperCamel: z.boolean().optional(), // I hope to have it DEPRECATED in the near future.
-  outFilePath: z.string().optional().default("./mysqlToZod"),
-  fileName: z.string().optional().default("schema.ts"),
+  output: z.object({
+    outDir: z.string().optional().default("./mysqlToZod"),
+    fileName: z.string().optional().default("schema.ts"),
+  }),
   dbConnection: z.any().optional(),
   tableNames: z.string().array().optional().default([]),
-  nullType: nullTypeUnionSchema, // I hope to have it DEPRECATED in the near future.
   comments: optionCommentsSchema.optional(),
   type: typeOptionSchema.optional(),
   schema: SchemaOptionSchema.optional(),
@@ -25,13 +22,11 @@ export const mysqlToZodOptionSchema = z.object({
 export type MysqlToZodOption = z.infer<typeof mysqlToZodOptionSchema>;
 
 export const basicMySQLToZodOption: MysqlToZodOption = {
-  isAddType: true,
-  isCamel: true,
-  isTypeUpperCamel: true,
-  outFilePath: "./mysqlToZod",
-  fileName: "schema.ts",
+  output: {
+    outDir: "./mysqlToZod",
+    fileName: "schema.ts",
+  },
   tableNames: [],
-  nullType: "nullable",
   comments: {
     table: {
       active: true,
