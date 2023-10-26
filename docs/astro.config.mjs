2be8b9d593cb81e614e22ea4895c18cc6209bb01
yuumillar <araera111@gmail.com>
2023-10-26 21:59:42 +0900

    feat:

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -3,16 +3,12 @@ import react from "@astrojs/react";
 import pandacss from "@pandacss/astro";
 import { defineConfig } from "astro/config";
 
-import cloudflare from "@astrojs/cloudflare";
-
 // https://astro.build/config
 export default defineConfig({
   markdown: {
     shikiConfig: {
-      theme: "solarized-dark"
-    }
+      theme: "solarized-dark",
+    },
   },
   integrations: [react(), mdx(), pandacss()],
-  output: "server",
-  adapter: cloudflare()
 });
