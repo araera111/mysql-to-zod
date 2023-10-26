518198be46ed3c52a37d11912b6962b67accb6e7
yuumillar <araera111@gmail.com>
2023-10-26 15:37:34 +0900

    feat:

--- docs/panda.config.ts
+++ docs/panda.config.ts
@@ -5,7 +5,12 @@ export default defineConfig({
   preflight: true,
 
   // Where to look for your css declarations
-  include: ["./src/**/*.{js,jsx,ts,tsx}", "./pages/**/*.{js,jsx,ts,tsx}"],
+  include: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./pages/**/*.{js,jsx,ts,tsx}",
+    "./src/**/*.{ts,tsx,js,jsx,astro}",
+    "./pages/**/*.{ts,tsx,js,jsx,astro}",
+  ],
 
   // Files to exclude
   exclude: [],
