c6c38767795feddabbad3b6a26992eb1b75a2bb5
yuumillar <araera111@gmail.com>
2023-10-26 15:55:02 +0900

    feat:

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -5,6 +5,8 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://araera111.github.io/",
+  base: "/mysql-to-zod",
   markdown: {
     shikiConfig: {
       theme: "solarized-dark",
