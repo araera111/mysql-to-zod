cad3be6c35832214a9b08f39e22010958e865c42
yuumillar <araera111@gmail.com>
2023-10-28 00:34:17 +0900

    feat:

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -1,27 +1,25 @@
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import starlight from "@astrojs/starlight";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', link: '/guides/example/' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  integrations: [
+    starlight({
+      title: "mysql-to-zod",
+      social: {
+        github: "https://github.com/araera111/mysql-to-zod",
+      },
+      sidebar: [
+        {
+          label: "Start Here",
+          items: [{ label: "Getting Started", link: "/" }],
+        },
+        {
+          label: "Options",
+          autogenerate: { directory: "options" },
+        },
+      ],
+      customCss: ["./src/styles/custom.css"],
+    }),
+  ],
 });
