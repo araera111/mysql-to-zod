d20a0d9e07404dc6ef83c6f383c4dc92ec806850
yuumillar <araera111@gmail.com>
2023-10-27 19:00:09 +0900

    feat:

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -1,14 +1,27 @@
-import mdx from "@astrojs/mdx";
-import react from "@astrojs/react";
-import pandacss from "@pandacss/astro";
-import { defineConfig } from "astro/config";
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-  markdown: {
-    shikiConfig: {
-      theme: "solarized-dark",
-    },
-  },
-  integrations: [react(), mdx(), pandacss()],
+	integrations: [
+		starlight({
+			title: 'My Docs',
+			social: {
+				github: 'https://github.com/withastro/starlight',
+			},
+			sidebar: [
+				{
+					label: 'Guides',
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{ label: 'Example Guide', link: '/guides/example/' },
+					],
+				},
+				{
+					label: 'Reference',
+					autogenerate: { directory: 'reference' },
+				},
+			],
+		}),
+	],
 });
