c27f7a34b9c56ed7234bca4b012583bdb2f233e2
yuumillar <araera111@gmail.com>
2023-10-29 00:28:25 +0900

    feat:

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -6,6 +6,17 @@ export default defineConfig({
   integrations: [
     starlight({
       title: "mysql-to-zod",
+      logo: { src: "./src/assets/logo.png" },
+      favicon: "./src/assets/logo.png",
+      locales: {
+        root: {
+          label: "日本語",
+          lang: "ja",
+        },
+        en: {
+          label: "English",
+        },
+      },
       social: {
         github: "https://github.com/araera111/mysql-to-zod",
       },
@@ -16,10 +27,18 @@ export default defineConfig({
         },
         {
           label: "Options",
-          autogenerate: { directory: "options" },
+          items: [
+            { label: "Config", link: "/options/config" },
+            { label: "DBConnection", link: "/options/db-connection" },
+            { label: "TableNames", link: "/options/table-names" },
+            { label: "Output", link: "/options/output" },
+            { label: "Comments", link: "/options/comments" },
+            { label: "Type", link: "/options/type" },
+            { label: "Schema", link: "/options/schema" },
+            { label: "Sync", link: "/options/sync" },
+          ],
         },
       ],
-      customCss: ["./src/styles/custom.css"],
     }),
   ],
 });
