48567848bbe6132245c471968c4ccf3b9b170129
yuumillar <araera111@gmail.com>
2023-12-29 23:16:27 +0900

    Refactor mysqlToZod.config.js

--- mysqlToZod.config.js
+++ mysqlToZod.config.js
@@ -1,40 +1,47 @@
 /** @type {import("./src/options/options").MysqlToZodOption} */
 const options = {
-  output: {
-    outDir: "./mysqlToZod",
-    fileName: "schema.ts",
-  },
-  comments: {
-    table: {
-      active: true,
-      format: "// [table:!name] : !text",
-    },
-    column: {
-      active: true,
-      format: "// !name : !text",
-    },
-  },
-  type: {
-    declared: "type",
-    format: "pascal",
-    prefix: "",
-    suffix: "",
-    replacements: [],
-  },
-  schema: {
-    format: "camel",
-    prefix: "",
-    suffix: "Schema",
-    replacements: [],
-    nullType: "nullish",
-    inline: true,
-    zod: {
-      implementation: [],
-      references: [],
-    },
-  },
-  sync: {
-    active: false,
-  },
+	output: {
+		outDir: "./mysqlToZod",
+		fileName: "schema.ts",
+	},
+	comments: {
+		table: {
+			active: true,
+			format: "// [table:!name] : !text",
+		},
+		column: {
+			active: true,
+			format: "// !name : !text",
+		},
+	},
+	type: {
+		declared: "type",
+		format: "pascal",
+		prefix: "",
+		suffix: "",
+		replacements: [],
+	},
+	schema: {
+		format: "camel",
+		prefix: "",
+		suffix: "Schema",
+		replacements: [],
+		nullType: "nullish",
+		inline: true,
+		zod: {
+			implementation: [],
+			references: [],
+		},
+	},
+	sync: {
+		active: false,
+	},
+	separate: {
+		isSeparate: true,
+		insertPrefix: "insert",
+		insertSuffix: "",
+		selectPrefix: "",
+		selectSuffix: "",
+	},
 };
 module.exports = options;
