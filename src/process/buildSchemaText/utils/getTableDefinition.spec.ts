a41078512698b1f2469df64074bfbe0b8b2effbb
yuumillar <araera111@gmail.com>
2024-01-10 00:02:10 +0900

    Refactor getTableDefinition function to use async/await and improve error handling

--- src/process/buildSchemaText/utils/getTableDefinition.spec.ts
+++ src/process/buildSchemaText/utils/getTableDefinition.spec.ts
@@ -1,14 +1,35 @@
+import { R } from "@mobily/ts-belt";
 import { getTableDefinition } from "./getTableDefinition";
 
-describe('getTableDefinition', () => {
-	it('case1', async() => {
+describe("getTableDefinition", () => {
+	it("case1 this test is local test...", async () => {
 		const tableName = "todo";
 		const dbConnection = "mysql://root:root@localhost:3306/my_todo";
 		const res = await getTableDefinition(tableName, dbConnection);
-		const result = [
-  "todo",
-  "CREATE TABLE `todo` (\n  `id` int NOT NULL AUTO_INCREMENT,\n  `task` varchar(255) NOT NULL,\n  `completed` tinyint(1) NOT NULL DEFAULT '0',\n  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,\n  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,\n  PRIMARY KEY (`id`)\n) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci"
-]
-		expect(res).toEqual(result)
+		const result = R.Ok([
+			"todo",
+			"CREATE TABLE `todo` (\n  `id` int NOT NULL AUTO_INCREMENT,\n  `task` varchar(255) NOT NULL,\n  `completed` tinyint(1) NOT NULL DEFAULT '0',\n  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,\n  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,\n  PRIMARY KEY (`id`)\n) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci",
+		]);
+		expect(res).toEqual(result);
+	});
+
+	it("case2 can not connect", async () => {
+		const tableName = "todo";
+		const dbConnection = "mysql://root:invalidPassword@localhost:3306/my_todo";
+		const res = await getTableDefinition(tableName, dbConnection);
+		const result = R.Error(
+			"getTableDefinitionError: Error: Access denied for user 'root'@'172.18.0.1' (using password: YES)",
+		);
+		expect(res).toEqual(result);
+	});
+
+	it("case3 table not found", async () => {
+		const tableName = "todoooo";
+		const dbConnection = "mysql://root:root@localhost:3306/my_todo";
+		const res = await getTableDefinition(tableName, dbConnection);
+		const result = R.Error(
+			"getTableDefinitionError: getTableDefinitionError: Error: Table 'my_todo.todoooo' doesn't exist",
+		);
+		expect(res).toEqual(result);
 	});
 });
--- src/process/buildSchemaText/utils/getTableDefinition.ts
+++ src/process/buildSchemaText/utils/getTableDefinition.ts
@@ -1,17 +1,36 @@
-import mysql from "mysql2/promise";
+import { AR, R, pipe } from "@mobily/ts-belt";
 import { z } from "zod";
 import { debugWriteFileSync } from "../../../utils/debugUtil";
+import { createConnection } from "../../getTables/utils/getTablesUtil";
 
 export const getTableDefinition = async (
 	tableName: string,
 	dbConnection: string,
-) => {
-	const connection = await mysql.createConnection(dbConnection);
-	const [table] = await connection.query("show create table ??", tableName);
-	if (!Array.isArray(table)) return [];
-	// biome-ignore lint/suspicious/noExplicitAny: <explanation>
-	const result = table.flatMap((x: any) => Object.values(x));
-	debugWriteFileSync(result);
-	await connection.destroy();
-	return z.string().array().parse(result);
-};
+) =>
+	pipe(
+		dbConnection,
+		createConnection,
+		AR.make,
+		AR.flatMap(async (x) => {
+			const q = async () => await x.query(`show create table ${tableName}`);
+			const p = await pipe(
+				x,
+				q,
+				AR.make,
+				AR.mapError((x) => `getTableDefinitionError: ${x}`),
+			);
+			await x.destroy();
+			return p;
+		}),
+		AR.flatMap(async (x) => {
+			const table = await x;
+			debugWriteFileSync(table);
+			if (!Array.isArray(table)) {
+				return R.Error("getTableDefinitionError: table is not array");
+			}
+			// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+			const result = table[0].flatMap((x: any) => Object.values(x));
+			return R.Ok(z.string().array().parse(result));
+		}),
+		AR.mapError((x) => `getTableDefinitionError: ${x}`),
+	);
